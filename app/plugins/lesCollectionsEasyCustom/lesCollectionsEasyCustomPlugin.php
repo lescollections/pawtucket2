42699ca2372e1b0d3f8bf0889509712ce8816baf
gautiermichelin <gm@ideesculture.com>
2015-07-01 17:45:11 +0200

    copyright

--- app/plugins/lesCollectionsEasyCustom/lesCollectionsEasyCustomPlugin.php
+++ app/plugins/lesCollectionsEasyCustom/lesCollectionsEasyCustomPlugin.php
@@ -55,7 +55,7 @@ class lesCollectionsEasyCustomPlugin extends BaseApplicationPlugin {
         define("__LESCOLLECTIONS_INTRO__",$va_infos->collectionintro);
         define("__LESCOLLECTIONS_LOGIN_REQUIS__",$va_infos->loginrequis);
         define("__LESCOLLECTIONS_CONTACT_INFORMATIONS__",$va_infos->contactinformations);
-        define("__LESCOLLECTIONS_COPYRIGHT_TEXT__",$va_infos->contactinformations);
+        define("__LESCOLLECTIONS_COPYRIGHT_TEXT__",$va_infos->copyright);
         // Simple constant definition function, so we return true in all cases
         return true;
     }
